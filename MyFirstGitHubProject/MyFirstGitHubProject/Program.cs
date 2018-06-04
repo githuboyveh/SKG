51d89e63c7b6d7e26fcf66c2fc8520be8908008c
Steven Gelb <foresthillsman@msn.com>
2018-06-04 10:05:51 -0400

    Change from Hello World! to Hello Damn World!

---
+++ MyFirstGitHubProject/MyFirstGitHubProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstGitHubProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
